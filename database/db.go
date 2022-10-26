3493916eafb213bc8e5ecbe17027499f9dbb7bcd
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-10-26 18:41:51 +0700

    add: database config

---
+++ database/db.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/fathoor/mygram-go/model"
+	_ "github.com/joho/godotenv/autoload"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB_HOST     = os.Getenv("DB_HOST")
+	DB_PORT     = os.Getenv("DB_PORT")
+	DB_USER     = os.Getenv("DB_USER")
+	DB_PASS     = os.Getenv("DB_PASS")
+	DB_NAME     = os.Getenv("DB_NAME")
+	DB_SSL_MODE = os.Getenv("DB_SSL_MODE")
+	APP_HOST    = os.Getenv("APP_HOST")
+	APP_PORT    = os.Getenv("APP_PORT")
+	JWT_SECRET  = os.Getenv("JWT_SECRET")
+	db          *gorm.DB
+	err         error
+)
+
+func StartDB() {
+	dsn := "host=" + DB_HOST + " user=" + DB_USER + " password=" + DB_PASS + " dbname=" + DB_NAME + " port=" + DB_PORT + " sslmode=" + DB_SSL_MODE + " TimeZone=Asia/Jakarta"
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	fmt.Println("Database connected")
+	db.Debug().AutoMigrate(model.User{}, model.Photo{}, model.Comment{}, model.SocialMedia{})
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
