8aaf193e4db2910b71fef18035dd0035e52597cd
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-10-26 19:17:37 +0700

    edit: start db & app

--- main.go
+++ main.go
@@ -1,3 +1,12 @@
 package main
 
-func main() {}
+import (
+	"github.com/fathoor/mygram-go/database"
+	"github.com/fathoor/mygram-go/router"
+)
+
+func main() {
+	database.StartDB()
+	r := router.StartApp()
+	r.Run(database.APP_HOST + ":" + database.APP_PORT)
+}
