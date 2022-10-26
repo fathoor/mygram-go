38c0b3a3f8a2b54137837ce0f2cf39dbaf55625a
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-10-26 19:53:23 +0700

    add: `UserLogin` endpoint

--- controller/user.go
+++ controller/user.go
@@ -42,3 +42,42 @@ func UserRegister(c *gin.Context) {
 		"username": User.Username,
 	})
 }
+
+func UserLogin(c *gin.Context) {
+	db := database.GetDB()
+	contentType := helper.GetContentType(c)
+	_, _ = db, contentType
+	User := model.User{}
+
+	if contentType == APP_JSON {
+		c.ShouldBindJSON(&User)
+	} else {
+		c.ShouldBind(&User)
+	}
+
+	password := User.Password
+
+	err := db.Debug().Where("email = ?", User.Email).Take(&User).Error
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Unauthorized",
+			"msg":   "Email or password is wrong",
+		})
+		return
+	}
+
+	if !helper.ComparePassword([]byte(User.Password), []byte(password)) {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Unauthorized",
+			"msg":   "Wrong password",
+		})
+		return
+	}
+
+	token := helper.GenerateToken(User.ID, User.Email)
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": token,
+	})
+}
