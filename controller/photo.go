28667d85f0cda12dfcdd65e84a943fdd4449b905
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-10-27 08:47:17 +0700

    add: `PhotoUpdate` endpoint

--- controller/photo.go
+++ controller/photo.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/fathoor/mygram-go/database"
@@ -87,4 +88,47 @@ func PhotoGetAll(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, Data)
+}
+
+func PhotoUpdate(c *gin.Context) {
+	db := database.GetDB()
+	contentType := helper.GetContentType(c)
+	auth := c.MustGet("auth").(jwt.MapClaims)
+	userId := int(auth["id"].(float64))
+	photoId, _ := strconv.Atoi(c.Param("photoId"))
+	Photo := model.Photo{}
+
+	Photo.ID = uint(photoId)
+	Photo.UserId = userId
+
+	if contentType == APP_JSON {
+		if err := c.ShouldBindJSON(&Photo); err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+	} else {
+		if err := c.ShouldBind(&Photo); err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+	}
+
+	err := db.Debug().Model(&Photo).Where("id = ? AND user_id = ?", photoId, userId).Updates(model.Photo{Title: Photo.Title, Caption: Photo.Caption, PhotoUrl: Photo.PhotoUrl}).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Bad Request",
+			"msg":   err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":         Photo.ID,
+		"title":      Photo.Title,
+		"caption":    Photo.Caption,
+		"photo_url":  Photo.PhotoUrl,
+		"user_id":    Photo.UserId,
+		"updated_at": Photo.UpdatedAt,
+	})
 }
